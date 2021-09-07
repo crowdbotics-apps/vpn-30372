3a2195421c9ef62471ebab8b8e7144e633f317f3
Crowdbotics <team@crowdbotics.com>
2021-09-07 09:11:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "vpn_30372Identifier",
+  url: "https://vpn_30372Identifier.botics.co",
+  api: "https://vpn_30372Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
